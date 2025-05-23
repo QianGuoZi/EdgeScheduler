299054c9cc5305743f61e3b85c0ff5633100505d
QianGuoZi <lianghuaqian@163.com>
2025-05-23 18:19:12 +0800

    修复多任务脚本bug

--- run_tasks.sh
+++ run_tasks.sh
@@ -14,21 +14,26 @@ TASK_ZIP="/home/qianguo/Edge-Scheduler/task.zip"
 BASE_DIR="/home/qianguo/Edge-Scheduler/Controller/dml_tool"
 PORT_BASE=6000
 
-# 上传 task.zip 并启动任务
-echo "Uploading task.zip..."
-curl -F "file=@$TASK_ZIP" http://localhost:3333/taskRequestFile
-
-echo "Starting task with ID=3..."
-curl "http://localhost:3333/startupTask?taskId=3"
 
 # 对每个 id 执行操作
 for (( id=START_ID; id<=END_ID; id++ ))
 do
     echo "Processing ID: $id"
 
+    # 上传 task.zip 并启动任务
+    echo "Uploading task.zip..."
+    curl -F "file=@$TASK_ZIP" http://localhost:3333/taskRequestFile
+
+    echo "Starting task with ID= $id..."
+    curl "http://localhost:3333/startupTask?taskId=$id"
+
     # 进入对应目录
     cd "${BASE_DIR}/${id}" || { echo "Directory ${BASE_DIR}/${id} not found"; exit 1; }
 
+    # 等待10秒
+    echo "Waiting 10 seconds before next step..."
+    sleep 5
+    
     # 运行 Python 配置脚本
     python3 dataset_conf.py -d gl_dataset.json -t "$id"
     python3 gl_structure_conf.py -s gl_structure.json -t "$id"
@@ -37,9 +42,6 @@ do
     curl "http://localhost:3333/conf/dataset?taskId=${id}"
     curl "http://localhost:3333/conf/structure?taskId=${id}"
 
-    # 等待10秒
-    echo "Waiting 10 seconds before next step..."
-    sleep 10
 
     # 发送启动任务请求
     TARGET_PORT=$((PORT_BASE + id))
